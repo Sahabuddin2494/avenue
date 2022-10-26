67ef3bcbe3cf6aec0a2277c2c8885116b864bcf1
sahabuddin <sagorkhan83495@gmail.com>
2022-10-26 13:18:47 +0600

    Avenue

---
+++ assets/js/click js/partner.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.partner_body').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed: 900,
+    arrows:false,
+}); 
+});
+
+// banner
+$(document).ready(function(){
+    $('.banner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed: 2500,
+    arrows:false,
+}); 
+});
